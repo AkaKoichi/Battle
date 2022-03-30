ad3a700c1ae65b20e0f8da85cd848f7e79c7a663
AkaKoichi <bruninhomartins240@gmail.com>
2022-03-30 12:26:40 +0100

    grid

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,14 +1,13 @@
 function setup(){
-    createCanvas(width,height)
-    canvas.parent("game")
+    createCanvas(1000,1000)
 }
 
 function draw(){
     background("black")
-    let tilesize = width/20;
+    let tilesize = 1000/20;
 
-    for(let y = 0 ; y < height;y += tilesize){
-        for(let x = 0 ; x < width;x += tilesize){
+    for(let y = 0 ; y < 1000;y += tilesize){
+        for(let x = 0 ; x < 1000;x += tilesize){
             rect(x,y,tilesize,tilesize)
         }
     }
