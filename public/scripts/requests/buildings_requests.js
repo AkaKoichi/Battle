6845a5fbdad8629475647bf4571d45537dec55e6
AkaKoichi <bruninhomartins240@gmail.com>
2022-04-23 13:52:44 +0100

    commit

--- public/scripts/requests/buildings_requests.js
+++ public/scripts/requests/buildings_requests.js
@@ -31,3 +31,23 @@ async function get_buildings_by_id(id) {
         console.log(err);
     }
 }   
+
+async function build(id,bld_id,bld_x,bld_y,bld_current_health) {
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaa')
+    try {
+        // TODO: Verify user information  and give errors
+        const response = await fetch(`/api/buildings/build/${id}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+              },
+            body: JSON.stringify({bld_id,bld_x,bld_y,bld_current_health}) 
+        });
+        var result= await response.json();
+        return {inserted: response.status==200 , result: result };
+    }   catch (err) {
+        // Treat 500 errors here
+        console.log(err);
+    }
+} 
