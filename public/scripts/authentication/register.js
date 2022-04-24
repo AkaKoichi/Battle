500cdd4013fc6e0ebaa7edeb41a8d16cbf1068e0
AkaKoichi <bruninhomartins240@gmail.com>
2022-04-24 02:15:47 +0100

    register inputs

--- public/scripts/authentication/register.js
+++ public/scripts/authentication/register.js
@@ -1,7 +1,37 @@
+let input_name;
+let input_password;
+
+function preload() {
+    register_button = createImg('/images/buttons/registo.png');
+    sound_button = createImg('/images/buttons/som.png');
+    music_button = createImg('/images/buttons/musica.png');
+}
+
+function setup() {
+    
+    input_name = createInput();
+    input_name.position(600, 300);
+    
+    input_password = createInput();
+    input_password.position(600, 370);
+    createCanvas(1420, 700);
+    //buttons
+    register_button.position(520, 480);
+    sound_button.position(1250, 650);
+    music_button.position(1350, 650);
+    register_button.mousePressed(register_user);
+}
+
+function draw() {
+    textSize(20);
+    text("User name:",600,270);
+    text("Password:",600,350);
+}
+
 async function register_user() {
     try {
-        let name = document.getElementById("name").value;
-        let password = document.getElementById("password").value;
+        let name = input_name.value();
+        let password = input_password.value();
         let user = {
             name: name,
             password: password
@@ -17,32 +47,5 @@ async function register_user() {
     } catch (err) {
         console.log(err);
     }
-}
-
-function preload() {
-    register_button = createImg('/images/buttons/registo.png');
-    sound_button = createImg('/images/buttons/som.png');
-    music_button = createImg('/images/buttons/musica.png');
-}
-
-function setup() {
-    let name;
-    name = createInput();
-    name.position(600, 300);
-    let password;
-    password = createInput();
-    password.position(600, 370);
-    createCanvas(1420, 700);
-    //buttons
-    register_button.position(520, 480);
-    sound_button.position(1250, 650);
-    music_button.position(1350, 650);
-    register_button.mousePressed(register_user);
-}
-
-function draw() {
-    textSize(20);
-    text("User name:",600,270);
-    text("Password:",600,350);
 }
 
