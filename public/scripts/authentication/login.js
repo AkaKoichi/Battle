b6017d00005d2fac1d5d8dad61c77c692f83b15c
Hundred10th <tomascant02001@hotmail.com>
2022-04-24 02:50:24 +0100

    p5 login/register

--- public/scripts/authentication/login.js
+++ public/scripts/authentication/login.js
@@ -12,3 +12,33 @@ async function login_user() {
         console.log(err)
     }
 }
+
+function preload() {
+    logo = createImg('/images/logo/logo.png');
+    login_button = createImg('/images/buttons/login.png');
+    register_button = createImg('/images/buttons/registo.png');
+    sound_button = createImg('/images/buttons/som.png');
+    music_button = createImg('/images/buttons/musica.png');
+}
+
+function setup() {
+    let name;
+    name = createInput();
+    name.position(600, 300);
+    let password;
+    password = createInput();
+    password.position(600, 370);
+    createCanvas(1420, 700);
+    //buttons
+    register_button.position(520, 480);
+    sound_button.position(1250, 650);
+    music_button.position(1350, 650);
+    login_button.mousePressed();
+    register_button.mousePressed();
+}
+
+function draw() {
+    textSize(20);
+    text("User name:",600,270);
+    text("Password:",600,350);
+}
